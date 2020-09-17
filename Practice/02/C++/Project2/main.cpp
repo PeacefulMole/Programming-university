32164480e3a70c42ff84157a312a834343421eb9
PeacefulMole <PeacefulMole@yandex.ru>
2020-09-17 09:31:54 +0300

    Добавлено 2 задание

---
+++ Practice/02/C++/Project2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int one = 3, two = 3.14;
+    double one = 3, two = 3.14;
+    cout <<one <<endl <<two <<endl;
+}
---
+++ Practice/02/Python/Project2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+one = 3
+two = 3.14
+print (one)
+print (two)

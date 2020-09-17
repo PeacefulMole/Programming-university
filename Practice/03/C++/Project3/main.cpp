8302c1367a890c441fe4a877e6a418c016785aff
PeacefulMole <PeacefulMole@yandex.ru>
2020-09-17 09:52:09 +0300

    Добавлено 3 задание

---
+++ Practice/03/C++/Project3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int prikol;
+    //int neprikol;
+    double prikol;
+    double neprikol;
+    cin >> prikol >> neprikol;
+    cout <<prikol+neprikol <<endl <<prikol-neprikol <<endl <<prikol*neprikol <<endl <<prikol/neprikol <<endl;
+    return 0;
+}
---
+++ Practice/03/Python/Project3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+a=float(input("Первое число: "))
+b=float(input("Второе число: "))
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
